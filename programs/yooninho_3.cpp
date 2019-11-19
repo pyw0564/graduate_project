ea357108c5aac74eae3c84cea1883546ad316446
pyw0564 <pyw0564@gmail.com>
2019-11-19 13:55:11 +0900

    tmp

---
+++ programs/yooninho_3.cpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+
+int main(){
+  puts("WELCOME TO CODERS");
+}
---
+++ programs/yooninho_5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int sum = 0;
+  int n;
+  while(cin >> n) {
+   sum += n; 
+  }
+  cout << sum;
+}
